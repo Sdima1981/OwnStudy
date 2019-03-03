d8e9463a5ca544b75cb035ab11917b8838e5115a
Sdima1981 <surigins@gmail.com>
2019-03-03 09:39:22 +0200

    Moving by lessons

--- src/lesson4/stock/Ibm.java
+++ src/lesson4/stock/Ibm.java
@@ -8,9 +8,9 @@ public class Ibm {
         ibm.setMax (150);
         ibm.setMin (150);
 
-        ibm.updatePrice (ibm, 150.37);
-        ibm.updatePrice (ibm,149.82);
-        ibm.updatePrice (ibm,150.12);
+        ibm.updatePrice ( 150.37);
+        ibm.updatePrice (149.82);
+        ibm.updatePrice (150.12);
         ibm.printInformation ();
     }
 }
